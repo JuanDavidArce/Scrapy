ac74553642e03225a8dfcdd83270ec04325bad53
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-04-10 18:01:09 -0500

    Saving information

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -10,26 +10,19 @@ class QuotesSpider(scrapy.Spider):
         'https://quotes.toscrape.com/page/1'
     ]
     def parse(self, response):
-        print('\n\n\n')
-        print('***********')
-
         title = response.xpath('//h1/a/text()').get()
-        print(f'Titulo : {title}')
-        print('\n\n')
-
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        print('Citas:')
-        for quote in quotes:
-            print(f'- {quote}')
-        print('\n\n')
-        
         top_ten_tags = response.xpath('//div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()').getall()
-        print('Top ten tags: ')
-        for tag in top_ten_tags:
-            print(f'-{tag}')
+        
+        return {
+            'title':title,
+            'quotes':quotes,
+            'top_ten_tags':top_ten_tags
+        }
+            
 
 
 
-        print('***********')
-        print('\n\n\n')
+        
+        
 
