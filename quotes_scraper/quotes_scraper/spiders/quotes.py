9566620e23940c8cd5a4e2aebbb8ecaf14b30456
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-04-10 17:52:39 -0500

    title, quotes and top ten tags xpath

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,13 +1,35 @@
 import scrapy
 
+# Titulo = //h1/a/text()
+# Citas = //span[@class="text" and @itemprop="text"]/text()
+# Top ten tags = //div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()
+
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
         'https://quotes.toscrape.com/page/1'
     ]
     def parse(self, response):
+        print('\n\n\n')
         print('***********')
-        print(response.status)
+
+        title = response.xpath('//h1/a/text()').get()
+        print(f'Titulo : {title}')
         print('\n\n')
+
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        print('Citas:')
+        for quote in quotes:
+            print(f'- {quote}')
+        print('\n\n')
+        
+        top_ten_tags = response.xpath('//div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        print('Top ten tags: ')
+        for tag in top_ten_tags:
+            print(f'-{tag}')
+
+
+
         print('***********')
-        print(response.headers)
+        print('\n\n\n')
+
