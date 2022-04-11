b7f1d7f890744a67ad9bf74a09d46b6ddeee3882
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-04-10 19:02:05 -0500

    response follow

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,24 +1,36 @@
+from gc import callbacks
 import scrapy
 
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags = //div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()
+# Next page button = //ul[@class="pager"]//li[@class="next"]/a/@href
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
         'https://quotes.toscrape.com/page/1'
     ]
+    
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
+
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
         top_ten_tags = response.xpath('//div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()').getall()
         
-        return {
+        yield {
             'title':title,
             'quotes':quotes,
             'top_ten_tags':top_ten_tags
         }
+
+        next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link,callback = self.parse)
             
 
 
